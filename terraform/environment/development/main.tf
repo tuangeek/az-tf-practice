2cccc77c62929950e130d87d23bf481aede16ea1
teampen <136991215+teampen@users.noreply.github.com>
2024-11-10 12:01:37 -0500

    adding terraform backend

--- terraform/environment/development/main.tf
+++ terraform/environment/development/main.tf
@@ -1,14 +1,7 @@
 # create the backend resource group and storage account
 
-locals {
-    environment = "development"
-    location    = "eastus"
-}
-
 module "az-backend" {
-    source = "../../az-backend"
-
-    # variables
-    location = local.location
-    environment = local.environment
+  source      = "../../az-backend"
+  location    = "development"
+  environment = "eastus"
 }
