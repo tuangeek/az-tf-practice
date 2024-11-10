0a124472d1be481a581af7dcf0d8be959e42af01
teampen <136991215+teampen@users.noreply.github.com>
2024-11-10 12:53:00 -0500

    adding terraform backend

--- terraform/az-backend/main.tf
+++ terraform/az-backend/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "azbackendstorage${var.environment}sa"
+  name                     = "tfbackend${var.environment}sa"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
