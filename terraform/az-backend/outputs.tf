d44bb6f7bc1bf854fe539f9063b02dfc44af6638
teampen <136991215+teampen@users.noreply.github.com>
2024-11-16 09:27:47 -0500

    adding more information

--- terraform/az-backend/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "azurerm_resource_group"{
-  value = azurerm_resource_group.rg
-}
--- terraform/environment/development/main.tf
+++
@@ -1,6 +0,0 @@
-# create the backend resource group and storage account
-module "az-backend" {
-  source      = "../../az-backend"
-  environment = "development"
-  location    = "eastus"
-}
